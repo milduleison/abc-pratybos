d92c25ec0420e71437fa0b6745868067e8fa5c2f
milduleison <milda.rimsiene83@gmail.com>
2017-09-16 14:49:11 +0300

    papildymas
    atliktos dvi uzduotys: is input nuskaityta info ir atvaizduota

--- isInput i array/script.js
+++ isInput i array/script.js
@@ -11,8 +11,43 @@ function pushData(){
         document.getElementById("spausdink").innerHTML = x;
 }
 
+//Pagal paskaita: Is input perkeliam duomenis i console
+let addDataButton = document.querySelector(".btn-save-data");
+
+
+let automobiliai = [{
+    "marke": "",
+    "modelis": "",
+    "metai": ""
+}];
+
+    let markeElement = document.querySelector("#marke");
+    let modelisElement = document.querySelector("#modelis");
+    let metaiElement = document.querySelector("#metai");
+
+    function addData(){
+        
+        let marke = markeElement.value;
+        let modelis = modelisElement.value;
+        let metai = metaiElement.value;
+  if (!marke || !modelis || !metai) {
+            alert("Neįvesti visi laukeliai");
+            return;
+        }
+
+        let duomenys = {
+            "marke": marke,
+            "modelis": modelis,
+            "metai": metai
+        }
+    
+       automobiliai.push(duomenys);
+       console.log("Duomenys:", duomenys.marke, duomenys.modelis, duomenys.metai);
+        document.write("Duomenys:" + " Automobilio marke - "+ duomenys.marke + " Automobilio modelis - " + duomenys.modelis + " Automobilio metai - " + duomenys.metai); 
+    };  
+    
 
 //Geles spalva<input type="text" name="text" class="ginput">
 //Geles kavpas<input type="text" name="text" class="ginput">   "pavadinismas": "",
     //"spalva": "",
-    //"kvapas": "",
+    //"kvapas": ""
